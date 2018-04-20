e03937d09034938d4e62a037cf6e258435c04839
Tam Huynh <tamhuynhqt@gmail.com>
2018-04-20 22:50:41 +1000

    - Add basic About and Contact US page

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -1,4 +1,17 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 @section('content')
-    
+<div><img src={!! url( 'public/images/test.webp') !!} class="img-fluid" alt="About Us"></div>
+<h2> About us</h2>
+<div class="row container-fluid">
+    <div class="col-lg-6 col-md-6 ml-auto">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio molestias cumque nemo blanditiis reprehenderit culpa
+            officiis, nostrum rerum dolor et veniam laboriosam ratione, tempore ipsa eaque tempora nesciunt voluptas fugit!</p>
+    </div>
+    <div class="col-lg-6 col-md-6 ml-auto">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, voluptatibus eius! Reiciendis alias consectetur magni
+            itaque quo numquam ut eligendi, fugit, aut fuga adipisci voluptas quod atque, ratione eum? Quae. Lorem ipsum
+            dolor sit amet consectetur, adipisicing elit. Rerum provident ipsum non iste quod nostrum numquam voluptatum
+            iure illum ea repudiandae quasi perspiciatis beatae tenetur nemo voluptatibus, nam hic odio.</p>
+    </div>
+</div>
 @endsection
--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -1,4 +1,50 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 @section('content')
-    
+<div class="container">
+       @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+    <form method="POST" action={!! url('contact') !!}  class="form-horizontal">
+        @csrf
+        <div class="form-group row">
+            <label class="control-label col-sm-2">Full Name</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" name="name" required>
+            </div>
+
+        </div>
+        <div class="form-group row">
+            <label class="control-label col-sm-2">Email</label>
+            <div class="col-sm-6">
+                <input type="email" class="form-control" name="email" required>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="control-label col-sm-2">Phone</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" name="phone">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="control-label col-sm-2">Message</label>
+            <div class="col-sm-6">
+                <textarea class="form-control" id="contactMessage" name="message" required> </textarea>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
 @endsection
